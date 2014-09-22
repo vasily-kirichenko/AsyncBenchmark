49e1e5ea5ee487f74a43325fcc144b899e793b19
vasily-kirichenko <vaskir@gmail.com>
2014-09-22 11:11:28 +0400

    load

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open System.Diagnostics
+
+[<EntryPoint>]
+let main argv = 
+    let n = int argv.[0]
+    let sw = Stopwatch.StartNew()
+    seq { for i in 1..n -> async { return i + i }}
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+    sw.Stop()
+    printfn "%d asyncs in %O (%f /sec)" n sw.Elapsed ((float n / float sw.ElapsedMilliseconds) * 1000.)
+    0    
